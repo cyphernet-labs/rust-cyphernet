26cc921fc20f0731605024039e442d1ffe501c29
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2023-01-10 12:03:30 +0100

    Remove unused code

--- src/noise/xk/framing.rs
+++ src/noise/xk/framing.rs
@@ -9,22 +9,6 @@ impl NoiseTranscoder<NoiseXkState> {
         let ephemeral_key = KeyPair::generate().sk;
         let state = NoiseXkState::new_initiator(local_key, remote_key, ephemeral_key);
         NoiseTranscoder { state }
-
-        /*
-        let mut data = vec![];
-        loop {
-            let (act, h) = handshake.next(&data)?;
-            handshake = h;
-            if let Some(ref act) = act {
-                connection.write_all(&*act)?;
-                if let NoiseXkState::Complete(transcoder) = handshake {
-                    break Ok(transcoder);
-                }
-                data = vec![0u8; handshake.data_len()];
-                connection.read_exact(&mut data)?;
-            }
-        }
-         */
     }
 
     #[cfg(feature = "keygen")]
@@ -32,22 +16,5 @@ impl NoiseTranscoder<NoiseXkState> {
         let ephemeral_key = KeyPair::generate().sk;
         let state = NoiseXkState::new_responder(local_key, ephemeral_key);
         NoiseTranscoder { state }
-
-        /*
-        let mut data = vec![0u8; handshake.data_len()];
-        connection.read_exact(&mut data)?;
-        loop {
-            let (act, h) = handshake.next(&data)?;
-            handshake = h;
-            if let NoiseXkState::Complete(transcoder) = handshake {
-                break Ok(transcoder);
-            }
-            if let Some(act) = act {
-                connection.write_all(&*act)?;
-                data = vec![0u8; handshake.data_len()];
-                connection.read_exact(&mut data)?;
-            }
-        }
-         */
     }
 }
